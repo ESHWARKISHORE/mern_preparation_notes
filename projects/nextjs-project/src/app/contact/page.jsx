c5603da7540c7553aa664a94adf78b1ed51b74a4
Esshwarkishore-R <80311386+Esshwarkishore-R@users.noreply.github.com>
2025-06-28 23:30:21 +0530

    Day 2 - nextjs-project - Added the basic file and add the two routes contact us and about for testing the routes

---
+++ projects/nextjs-project/src/app/contact/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Contact = () => {
+  return (
+    <div>Contact
+      <Link href="/">Back to Home</Link></div>
+  )
+}
+
+export default Contact        
