9f76b536f1cd2997dd7ccd6d37fd64af79977070
Esshwarkishore-R <80311386+Esshwarkishore-R@users.noreply.github.com>
2025-07-08 13:38:01 +0530

    Day 6 - Added graphQL for the sample express server

--- projects/express-node-workout/index.js
+++ projects/express-node-workout/index.js
@@ -1,9 +1,12 @@
-const express  = require('express');
+const express = require('express');
 const cors = require('cors')
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const data = [
     {
@@ -18,25 +21,54 @@ const data = [
     },
 ]
 
-app.get('/get',(req, res)=>{
+app.get('/get', (req, res) => {
     res.status(200).send({
-        message: 'Hello this is express server', 
+        message: 'Hello this is express server',
         data: data
     });
 })
 
-app.post('/post', (req, res)=>{
+app.post('/post', (req, res) => {
     const newData = req.body;
     data.push(newData);
     res.status(200).send({
         message: "Data received successfully",
         updatedData: data,
-    }) 
+    })
 })
 
+const schema = buildSchema(`
+    type Query {
+    getData: [User]
+    } 
 
+type Mutation {
+    addUser(name: String!, age:String!, email: String!): [User] 
+    }
+
+  type User {
+    name: String
+    age: String
+    email: String
+  }
+`)
+
+const root = {
+    getData: () => data,
+    addUser: ({name, age, email}) => {
+        const newUser = {name, age, email}
+        data.push(newUser);
+        return data;
+    }
+}
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true 
+}));
 
 const PORT = 4000;
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log('Server is running in the PORT 4000')
 })
